e32267b4a1ddb12128729f2aed3310f912f3bb84
Vadim <vadim_1423@mail.ru>
2020-01-13 11:10:09 +0300

    crete

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// Соединение, выбор базы данных
+$pgsql_conn = pg_connect("host=ec2-174-129-33-107.compute-1.amazonaws.com dbname=d2vsnkphe5a3oj user=rtokoowoircggm password=7b7b20b8a7e3719a92a0f789626bfcd89b12e39de5e69bef5e706717a99eab24")
+    or die('Не удалось соединиться: ' . pg_last_error());
+
+if ($pgsql_conn) {
+    echo "Успешно подключились к : " . pg_host($pgsql_conn) . "<br/>\n";
+    $query = "show databases like 'db_name' ";
+    $result = pg_query($query) or die("Нету таблицы");//die('Ошибка запроса: ' . pg_last_error());
+    if($result != null)
+        echo "Таблица существует";
+    else
+        echo "Нету таблицы";
+    pg_close($dbconn);
+    
+} else {
+    echo "Bad connect :(";
+    print pg_last_error($pgsql_conn);
+    exit;
+}
+// Выполнение SQL-запроса
+//$query = 'SELECT * FROM authors';
+//$result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
+
+// Вывод результатов в HTML
+//echo "<table>\n";
+//while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+//    echo "\t<tr>\n";
+//    foreach ($line as $col_value) {
+//        echo "\t\t<td>$col_value</td>\n";
+//    }
+//    echo "\t</tr>\n";
+//}
+//echo "</table>\n";
+
+// Очистка результата
+//pg_free_result($result);
+
+// Закрытие соединения
+//pg_close($dbconn);
+?>
