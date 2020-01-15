0ab042facc15dccf8b97906735222d7c9c63330b
Vadim <vadim_1423@mail.ru>
2020-01-16 01:55:14 +0300

    test Проверка работы условия с таблицей

--- connect.php
+++ connect.php
@@ -11,9 +11,9 @@ if ($pgsql_conn) {
     $query = "show databases like 'customers' ";
     $query = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='customers'";
     $result=pg_query($pgsql_conn, $query);
-    echo "<br/>" + "-> ";
+    echo "Пишу: " + "-> ";
     //echo "<br/>" + $result;
-    
+
    // $query = 'DELETE FROM customers;';
    // $result = pg_query($query) or die('Ошибка запроса: ' . pg_last_error());
 
