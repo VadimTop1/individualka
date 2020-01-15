7858fea87316812c65006174c48fccce659df80e
Vadim <vadim_1423@mail.ru>
2020-01-16 01:33:50 +0300

    test Проверка работы условия с таблицей

--- connect.php
+++ connect.php
@@ -4,12 +4,16 @@ $pgsql_conn = pg_connect("host=ec2-174-129-33-107.compute-1.amazonaws.com dbname
     or die('Не удалось соединиться: ' . pg_last_error());
 
 $table;
-$strTable = "CREATE TABLE customers(Id SERIAL PRIMARY KEY,FirstName CHARACTER VARYING(30),LastName CHARACTER VARYING(30),Email CHARACTER VARYING(30),Age INTEGER);";
+//$strTable = "CREATE TABLE customers(Id SERIAL PRIMARY KEY,FirstName CHARACTER VARYING(30),LastName CHARACTER VARYING(30),Email CHARACTER VARYING(30),Age INTEGER);";
 
 if ($pgsql_conn) {
     echo "Успешно подключились к : " . pg_host($pgsql_conn) . "<br/>\n";
     $query = "show databases like 'customers' ";
-    $result=pg_query($pgsql_conn, $strTable);
+    $query = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='customers'";
+    $result=pg_query($pgsql_conn, $query);
+    echo "<\br> ->\t" + $result;
+    //$result=pg_query($pgsql_conn, $strTable);
+
     //result = pg_query($query) or die("Нету таблицы");//die('Ошибка запроса: ' . pg_last_error());
     ///echo $result;
     ///if($result != null)
