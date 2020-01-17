80efa9a38efc209fb6253f3e7144d526dd4da1be
Vadim <vadim_1423@mail.ru>
2020-01-17 07:47:15 +0300

    Проверка системы ()

--- testfile.php
+++ testfile.php
@@ -1,10 +1,22 @@
 <?php
-    # Если кнопка нажата
+    include "index.php";
     if( isset( $_POST['nazvanie_knopki'] ) )
     {
-        # Тут пишете код, который нужно выполнить.
-        # Пример:
-        echo 'Кнопка нажата!';
+        
+        echo 'Данные отправлены!';
+
+        if($obj_ControllerBD->OutPutDataTable($obj_Table1) == false)
+            echo "<br/><br/>-> Не удалось вывисти данные ";
+        else
+        {
+            echo "<br/><br/>";
+            foreach($obj_Table1 as $value)
+            {
+                echo $value->get_firstName() . " ";
+                echo $value->get_secondName() . " ";
+                echo $value->get_age() . "<br/>";
+            }
+        }
     }
 ?>
 
@@ -17,6 +29,6 @@
     <input type="text" size="30">
     <p><b>Сколько лет:</b><br>
     <input type="text" size="3">
-    <br/>
+    <br/><br/>
     <input type="submit" name="nazvanie_knopki" value="Нажмите" />
 </form>
