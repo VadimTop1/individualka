d342ae473059bc2d8a34ce99f89cfd8e8a2866f3
Vadim <vadim_1423@mail.ru>
2020-01-17 08:14:40 +0300

    Проверка системы ()

--- testfile.php
+++ testfile.php
@@ -2,35 +2,10 @@
 
 include "index.php";
 
-//use WorkWithBD\ClassBD\Table1\Table1;
-//use WorkWithBD\ControllerBD\ControllerBD;
-//use src\Param\Param;
-
-/*$host="ec2-174-129-33-107.compute-1.amazonaws.com";
-$dbname="d2vsnkphe5a3oj";
-$user="rtokoowoircggm";
-$password="7b7b20b8a7e3719a92a0f789626bfcd89b12e39de5e69bef5e706717a99eab24";
-
-$obj_ControllerBD = new ControllerBD($host,$dbname,$user,$password);*/
-
 if( isset( $_POST['nazvanie_knopki'] ) )
-    {
-        //$obj_Table1 = new Table1;
-
-        if($obj_ControllerBD->OutPutDataTable($obj_Table1) == false)
-            echo "<br/><br/>-> Не удалось вывисти данные ";
-        else
-        {
-            //Param::$obj_Table1 = $obj_Table1;
-            echo "<br/><br/>";
-            foreach($obj_Table1 as $value)
-            {
-                echo $value->get_firstName() . " ";
-                echo $value->get_secondName() . " ";
-                echo $value->get_age() . "<br/>";
-            }
-        }
-    }
+{
+        
+}
 ?>
 
 <form method="POST">
