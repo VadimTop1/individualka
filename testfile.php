3c14c1c99185490e6053e00b9e5dca95199aa21d
Vadim <vadim_1423@mail.ru>
2020-01-17 08:58:22 +0300

    Проверка системы ()

--- testfile.php
+++ testfile.php
@@ -1,17 +1,32 @@
 <?php
-   
-    
+
+include "index.php";
+
+if( isset( $_POST['nazvanie_knopki'] ) )
+{
+    $arrData = [
+        "'".$_POST['firstName']."'",
+        "'".$_POST['secondName']."'",
+        "'".$_POST['thirdName']."'",
+        "'".$_POST['age']."'"
+    ];
+    echo $arrData[0]." ". $arrData[1] . " " . $arrData[2]. " " . $arrData[3];
+    if($obj_ControllerBD->AddDataTable($obj_Table1,$arrData) != false)
+        echo "<br/>-> Успешно добавленно ";
+    else
+        echo "<br/><br/>-> Не удалось добавить ";
+}
 ?>
 
-<form action="update.php" method="post">
+<form method="POST">
     <p><b>Ваше Имя:</b><br>
-    <input type="firstname" size="30">
+    <input type="firstName" size="30">
     <p><b>Ваше Фамилия:</b><br>
-    <input type="secondname" size="30">
+    <input type="secondName" size="30">
     <p><b>Ваше Очество:</b><br>
-    <input type="thirdname" size="30">
+    <input type="thirdName" size="30">
     <p><b>Сколько лет:</b><br>
     <input type="age" size="3">
     <br/><br/>
-    <input type="submit" value="Нажмите" />
+    <input type="submit" name="nazvanie_knopki" value="Нажмите" />
 </form>
