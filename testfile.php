0e59612b238af9adddcca8b51971524aaedc38d2
Vadim <vadim_1423@mail.ru>
2020-01-17 08:56:10 +0300

    Проверка системы ()

--- testfile.php
+++ testfile.php
@@ -5,7 +5,7 @@
 
 <form action="update.php" method="post">
     <p><b>Ваше Имя:</b><br>
-    <input type="secondname" size="30">
+    <input type="firstname" size="30">
     <p><b>Ваше Фамилия:</b><br>
     <input type="secondname" size="30">
     <p><b>Ваше Очество:</b><br>
