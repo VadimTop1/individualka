7f52cb7ef28684bc168e93dcd8c6e979c8eeb95c
Vadim <vadim_1423@mail.ru>
2020-01-17 07:41:23 +0300

    Проверка системы ()

--- testfile.php
+++ testfile.php
@@ -1,3 +1,22 @@
 <?php
-echo "lol";
+    # Если кнопка нажата
+    if( isset( $_POST['nazvanie_knopki'] ) )
+    {
+        # Тут пишете код, который нужно выполнить.
+        # Пример:
+        echo 'Кнопка нажата!';
+    }
 ?>
+
+<form method="POST">
+    <p><b>Ваше Имя:</b><br>
+    <input type="text" size="30">
+    <p><b>Ваше Фамилия:</b><br>
+    <input type="text" size="30">
+    <p><b>Ваше Очество:</b><br>
+    <input type="text" size="30">
+    <p><b>Сколько лет:</b><br>
+    <input type="text" size="3">
+
+    <input type="submit" name="nazvanie_knopki" value="Нажмите" />
+</form>
