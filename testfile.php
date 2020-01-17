5d733e795259aaf887ea8cfe0139fdedf6462854
Vadim <vadim_1423@mail.ru>
2020-01-17 09:09:05 +0300

    Проверка системы ()

--- testfile.php
+++ testfile.php
@@ -2,13 +2,13 @@
 
 include "index.php";
 
-if( isset( $_POST['nazvanie_knopki'] ) && isset($_POST['firstName'] ) && isset($_POST['secondName'] ) && isset($_POST['thirdName'] ) && isset($_POST['age'] ) )
+if( isset( $_POST['nazvanie_knopki'] ) && isset($_POST["firstName"] ) && isset($_POST["secondName"] ) && isset($_POST["thirdName"] ) && isset($_POST["age"] ) )
 {
     $arrData = [
-        "'".(string)$_POST['firstName']."'",
-        "'".(string)$_POST['secondName']."'",
-        "'".(string)$_POST['thirdName']."'",
-        (int)$_POST['age']
+        "'".$_POST['firstName']."'",
+        "'".$_POST['secondName']."'",
+        "'".$_POST['thirdName']."'",
+        $_POST['age']
     ];
     //echo $arrData[0]." ". $arrData[1] . " " . $arrData[2]. " " . $arrData[3];
     if($obj_ControllerBD->AddDataTable($obj_Table1,$arrData) != false)
@@ -20,13 +20,13 @@ if( isset( $_POST['nazvanie_knopki'] ) && isset($_POST['firstName'] ) && isset($
 
 <form method="POST">
     <p><b>Ваше Имя:</b><br>
-    <input type="firstName" size="30">
+    <input type="text" name="firstName" size="30">
     <p><b>Ваше Фамилия:</b><br>
-    <input type="secondName" size="30">
+    <input type="text" name="secondName" size="30">
     <p><b>Ваше Очество:</b><br>
-    <input type="thirdName" size="30">
+    <input type="text" name="thirdName" size="30">
     <p><b>Сколько лет:</b><br>
-    <input type="age" size="3">
+    <input type="text" name="age" size="3">
     <br/><br/>
-    <input type="submit" name="nazvanie_knopki" value="Нажмите" />
+    <input type="submit" value="Нажмите" />
 </form>
