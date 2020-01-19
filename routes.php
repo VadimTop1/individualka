a6d141dc186e2b7d40b33211a29724494e1950b7
Vadim <vadim_1423@mail.ru>
2020-01-19 07:55:50 +0300

    Проверка роутинга ()

--- routes.php
+++ routes.php
@@ -3,7 +3,8 @@
     return [
         //'~^Page/(.*)$~' => [\Project\Controllers\MainController::class, 'sayHello'],
         //'~^page/(.*)$~' => [Controllers\ControllerP1::class, 'Show'],
-        '~^page/ВолковскийВадимСергеевич$~' => [ControllerP1::class, 'Show'],
+        '~^page/$~' => [ControllerP1::class, 'Show'],
+        //'~^page/Волковский_Вадим_Сергеевич$~' => [ControllerP1::class, 'Show'],
         //'~^test$~' => 
         //'~^users/authorization~' => [\Project\Controllers\ControllerMain::class, 'distributeUrl'],
         //'~^(.*)$~' => [\Project\Controllers\ControllerMain::class, 'distributeUrl']
