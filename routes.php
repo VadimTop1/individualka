bf685d640de9fedef6bde705017b84546b82696f
Vadim <vadim_1423@mail.ru>
2020-01-19 09:18:59 +0300

    Проверка роутинга ()

--- routes.php
+++ routes.php
@@ -3,15 +3,9 @@
     use Controllers\ControlerOutput\ControlerOutput;
 
     return [
-        //'~^Page/(.*)$~' => [\Project\Controllers\MainController::class, 'sayHello'],
-        //'~^page/(.*)$~' => [Controllers\ControllerP1::class, 'Show'],
-        '~^page/(.*)$~' => [ControllerP1::class, 'Main'],
-        //asd
+        //'~^page/(.*)$~' => [ControllerP1::class, 'Main'],
+        '~^page/Волковский_Вадим_Сергеевич$~' => [ControllerP1::class, 'Main'],
         '~^page/addInfom$~' => [ControlerOutput::class, 'Add'],
         '~^page/showInfom$~' => [ControlerOutput::class, 'Show']
-        //'~^test$~' => 
-        //'~^users/authorization~' => [\Project\Controllers\ControllerMain::class, 'distributeUrl'],
-        //'~^(.*)$~' => [\Project\Controllers\ControllerMain::class, 'distributeUrl']
-        //'~^authorization$~' => [\Project\Controllers\ControllerMain::class, 'distributeUrl']
     ];
 ?>
