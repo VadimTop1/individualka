2964a07eb0ba010a2893306470721af35922617c
Vadim <vadim_1423@mail.ru>
2020-01-19 08:24:11 +0300

    Проверка роутинга ()

--- app/Controllers/ControllerP1/ControllerP1.php
+++ app/Controllers/ControllerP1/ControllerP1.php
@@ -5,8 +5,23 @@ class ControllerP1
 {
     public function Show(array $param)
     {
-        $res = explode("/", $param[0])[1];
-        echo $res;
+        $titleNameWork =    "--> Индивидуальная работа";
+        $titleTheme =       "--> Разработка облачного приложения, работающего с БД.";
+        $titleTCondition =  "--> Разработать приложение, которое работает с базой данных, состоящей из одной таблицы<Br/>
+                            с четырьмя полями (информацию, которая хранится в БД выбрать самостоятельно).<Br/> 
+                            Интерфейс приложения должен содержать  две html-страницы.<Br/> 
+                            Первая выводит содержимое БД в виде таблицы, вторая содержит форму, позволяющую<Br/>
+                            ввести данные в таблицу.";
+        $titleNameGroup = "Ученик группы ПИм-19";
+        $resParam = explode("/", $param[0])[1];
+        $Nameauthor = explode("_", $param[0]);
+        $titleNameauthor = $Nameauthor[0] . " " . $Nameauthor[1] . " " . $Nameauthor[2];
+
+        echo $titleNameWork."<br/>";
+        echo $titleTheme."<br/>";
+        echo $titleTCondition."<br/>";
+        echo $titleNameGroup."<br/>";
+        echo $titleNameauthor."<br/>";
     }
 }
 
