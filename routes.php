b69ae975454ad66d49ef836f88d9ff6d9a109090
Vadim <vadim_1423@mail.ru>
2020-01-22 13:20:05 +0300

    Проверка работы ООП

--- routes.php
+++ routes.php
@@ -4,11 +4,7 @@
 
     return [
         //'~^page/(.*)$~' => [ControllerP1::class, 'Main'],
-        '~^page/Волковский_Вадим_Сергеевич$~' => [ControllerP1::class, 'Main'],
-        '~^page/Дмитрий_Белкин_Геннадьиевич$~' => [ControllerP1::class, 'Main'],
-        '~^page/Максим_Висягин_Александрович$~' => [ControllerP1::class, 'Main'],
-        '~^page/addInfom$~' => [ControlerOutput::class, 'Add'],
-        '~^page/showInfom$~' => [ControlerOutput::class, 'Show'],
-        '~^page/updateInfom$~' => [ControlerOutput::class, 'Update']
+        '~^page/reg$~' => [ControllerP1::class, 'Main'],
+        '~^page/log$~' => [ControllerP1::class, 'Main']
     ];
 ?>
